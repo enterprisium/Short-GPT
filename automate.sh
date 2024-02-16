930f4d0b6ebcbdcbb986036824d28ace40bb6bd7
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-02-16 11:37:45 -0800

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -2,7 +2,7 @@
 
 log=$(date +%s)
 rm *.log
-python3 ultra.py >> $log.log &&
-python3 final_upload.py >> $log.log &&
+python ultra.py >> $log.log &&
+python final_upload.py >> $log.log &&
 
 echo "run"
